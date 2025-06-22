e63d322b43792eb9d26f572629bd9c2d4338ee22
Ayushma <ayushmatripathi2002.com>
2025-06-22 12:29:50 +0530

    added middleware in attendance router

--- src/attendence/router.js
+++ src/attendence/router.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { markStudentAttendance } from './controller.js';
+import { authorizeRoles, verifyToken } from '../middleware/auth.js';
+
 
 const app = express();
 
-app.post('/student-attendence',markStudentAttendance)
+app.post('/student-attendence',verifyToken,authorizeRoles("admin","teacher"),markStudentAttendance)
 
 //testing 2 in development
 
