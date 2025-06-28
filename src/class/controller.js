dfbeb2df7cb0a56ff6301eb5cabc51feb1cdda34
Ayushma0208 <ayushmatripathi2002@gmail.com>
2025-06-28 11:41:44 +0530

    added class assign to teacher

--- src/class/controller.js
+++ src/class/controller.js
@@ -1,4 +1,4 @@
-import { insertClass } from "./model.js";
+import { AssignTecherToClass, insertClass } from "./model.js";
 
 export const addClass = async(req,res) =>{
     const {classname} = req.body;
@@ -7,4 +7,13 @@ export const addClass = async(req,res) =>{
   }
     const data = await insertClass(classname.trim())
     return res.status(200).json({message:"Class added successfully",class:data.rows[0]})
+}
+
+export const assignClass = async(req,res) =>{
+  const { teacher_id, class_id } = req.body;
+  if (!teacher_id || !class_id) {
+    return res.status(400).json({ message: 'Teacher ID and Class ID are required' });
+  }
+  const result = await AssignTecherToClass(teacher_id, class_id);
+  res.status(201).json({ message: "Teacher assigned successfully", data: result.rows[0] });
 }
--- src/class/router.js
+++ src/class/router.js
@@ -1,8 +1,11 @@
 import express from 'express';
-import { addClass} from './controller.js';
+import { addClass, assignClass} from './controller.js';
+import { authorizeRoles, verifyToken } from '../middleware/auth.js';
 
 const app = express();
 
-app.post('/add-class',addClass)
+app.post('/add-class',verifyToken,authorizeRoles("admin"),addClass)
+
+app.post('/assign-teacher-to-class', assignClass)
 
 export default app;
